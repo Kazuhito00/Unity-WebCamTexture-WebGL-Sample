63859915185b6e080188c6556f508484949c4bda
KazuhitoTakahashi <shigeki.takahashi.dev@gmail.com>
2021-01-26 01:14:10 +0900

    Initial commit

---
+++ Assets/WebCamController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamController : MonoBehaviour
+ {
+    int width = 960;
+    int height = 540;
+    int fps = 30;
+    WebCamTexture webcamTexture;
+
+    void Start () 
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        webcamTexture = new WebCamTexture(devices[0].name, this.width, this.height, this.fps);
+        GetComponent<Renderer> ().material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
